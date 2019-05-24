bf4ad5b92c2b7a0e4f2a517c10c142b8a7b661e9
ilman teguh prasetya <ilmanfrasetya@gmail.com>
2019-05-24 09:29:02 +0700

    function and loop

---
+++ src/belajarFunction.py
@@ -0,0 +1,10 @@
+from __future__ import print_function
+
+def getHelloWorld():
+    print('Hello World')
+
+def main():
+    getHelloWorld()
+
+if __name__ == "__main__":
+    main()
--- src/belajarPengulangan.py
+++ src/belajarPengulangan.py
@@ -1,1 +1,8 @@
-from __future__ import print function
+from __future__ import print_function
+
+def main():
+    varibaleInput = int(input("varibale : "))
+    for i in range(5):
+        print(varibaleInput)
+if __name__ == "__main__":
+    main()
